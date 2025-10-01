32867b72fa42e1403aa6f62515424b9e546d0bbc
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-10-01 10:52:39 -0300

    Realizado instalação do Express, cors, body-parse, prisma e prismaclient

--- model/DAO/filme.js
+++ model/DAO/filme.js
@@ -12,22 +12,54 @@
 
 /********************************************************************************************
 ********************************* COMANDOS UTILIZADOS ***************************************
-* forEach - Método de um array que percorre todos os indices.
-* push - Método de um array para adicionar no final elementos dentro de um array.
-* find - Método de um array para procurar um elemento especifico pelo conteudo.
-Retorna somente o primeiro elemento que satisfaz a condição.
-* filter- Método de um array para procurar um elemento especifico pelo conteudo.
-Retorna um novo array com todos os elementos que satisfazem a condição.
+* $queryRawUnsafe(script) -> permite executar um script SQL de uma variavel e que retorna 
+valores de um banco (SELECT).
+* $executeRawUnsafe(script) -> Permite executar um script SQL de uma variavel e que não 
+retorna dados do banco (INSERT, UPDATE e DELETE.)
+* $queryRaw(script) -> permite executar um script SQL sem estar em uma variavel e que retorna 
+valores do banco (SELECT). Faz tratamentos de segurança contra SQL Injection
+* $executeRaw(script) -> Permite executar um script SQL sem estar em uma variavel e que não 
+retorna dados do banco (INSERT, UPDATE e DELETE.). Faz tratamentos de segurança contra SQL 
+Injection
 ************************************** OBSERVAÇÕES ******************************************
 * Todo array possui um indice, e para acessar um indice devemos dizer qual a sua posição [0],
 [3] ou [n], seria a posição em que aquele elemento se encontra dentro de todo array.
 * Todo Json pode ser acessado com ".nomeAtributo".
+********************************* EXEMPLOS BIBLIOTECAS **************************************
+BANCO RELACIONAL
+* Sequelize -> Foi Utilizado em muitos projetos desde o inicio do node.
+* Prisma -> É uma dependencia atual que trabalha com BD  (MYSQL, PostgreSQL, SQL Server), (SQL ou ORM).
+* Knex -> É uma dependencia para trabalhar com BD (MYSQL)
+BANCO NÃO RELACIONAL
+* Mongoose -> É uma dependencia para o Mongo DB (Não relacional).
+
 ******************************** BIBLIOTECAS UTILIZADAS *************************************
 
 ********************************************************************************************/
 
+// Import da dependência do Prisma que permite a execução de script SQL no BD
+const { PrismaClient } = require('@prisma/client')
+
+// Cria um novo objeto baseado na classe do PrismaClient
+const prisma = new PrismaClient()
+
 // Retorna uma lista de todos os filmes do banco de dados
 async function getSelectAllMovies() {
+    try {
+        // Variavel com o comando sql para buscar toda a tabela de filme
+        let sql = `select * from tbl_filme order by id desc`
+        // Variavel para inserir o comando no banco de dados
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if (result.length > 0)
+            return result
+        else
+            return false
+
+
+    } catch (error) {
+        return false
+    }
 
 }
 // Retorna um filme filtrando pelo ID do banco de dados
@@ -48,4 +80,8 @@ async function setUpdateMovie(id) {
 // Excluir um filme pelo ID no banco de dados
 async function setDeleteMovie(id) {
 
+}
+
+module.exports = {
+    getSelectAllMovies
 }
