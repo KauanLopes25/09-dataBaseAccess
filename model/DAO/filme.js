2f2f6f304ea237eb1af048b0d31d9dbd870293c2
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-10-01 08:47:17 -0300

    Estrutura básica para requisições e respostas entre a tabela filme

---
+++ model/DAO/filme.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/********************************************************************************************
+* Objetivo: Arquivo para comunicação com a tabela Filme.
+* Autor: Kauan Lopes Pereira
+* Data: 01/10/2025
+* Versão: 1.0.10.25
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+* forEach - Método de um array que percorre todos os indices.
+* push - Método de um array para adicionar no final elementos dentro de um array.
+* find - Método de um array para procurar um elemento especifico pelo conteudo.
+Retorna somente o primeiro elemento que satisfaz a condição.
+* filter- Método de um array para procurar um elemento especifico pelo conteudo.
+Retorna um novo array com todos os elementos que satisfazem a condição.
+************************************** OBSERVAÇÕES ******************************************
+* Todo array possui um indice, e para acessar um indice devemos dizer qual a sua posição [0],
+[3] ou [n], seria a posição em que aquele elemento se encontra dentro de todo array.
+* Todo Json pode ser acessado com ".nomeAtributo".
+******************************** BIBLIOTECAS UTILIZADAS *************************************
+
+********************************************************************************************/
