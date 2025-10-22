2cce0bbc0a65ebd1a0c7d6959a0ec31aa012f7e8
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-10-22 11:22:59 -0300

    ROUTES
    Arquivo rotas da tbl_filme finalizado

--- controller/filme/router_filme.js
+++ controller/filme/router_filme.js
@@ -22,6 +22,11 @@
 const express = require('express')
 // Responsável pelas permissões da API (APP)
 const cors = require('cors')
+// Responsável por gerenciar a chegada dos dados da api com o front
+const bodyParser = require('body-parser')
+
+// Criando um objeto especialista no formato JSON para receber dados via POST e PUT
+const bodyParserJSON = bodyParser.json()
 
 const router = express.Router();
 
@@ -29,15 +34,15 @@ const router = express.Router();
 const filmeController = require('../filme/controller_filme.js')
 const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
 
-// 1°
+// 1° LISTAR
 router.get('/', cors(), async function (request, response){
     // Chama a função para listar os filmes do BD
     let filme = await filmeController.listarFilme()
     response.status(filme.status_code)
     response.json(filme)
-    console.log('ENDPOINT 1° tbl_filme - Requisitado')
+    console.log('ENDPOINT 1° - Requisitado na tbl_filme')
 })
-// 2°
+// 2° BUSCAR POR ID
 router.get('/:id', cors(), async function (request, response){
     // Recebe o ID encaminhado via parametro na requisição
     let idFilme = request.params.id
@@ -45,7 +50,42 @@ router.get('/:id', cors(), async function (request, response){
     let filme = await filmeController.buscarFilmeId(idFilme)
     response.status(filme.status_code)
     response.json(filme)
-    console.log('ENDPOINT 2° tbl_filme - Requisitado')
+    console.log('ENDPOINT 2° - Requisitado na tbl_filme')
 })
-
+// 3° INSERIR
+router.post('/', cors(), bodyParserJSON, async function (request, response){
+    // Recebe os dados do body da requisição (Se você utilizar o bodyParser, é obrigatório ter no endPoint)
+    let dadosBody = request.body
+    let contentType = request.headers['content-type']
+    // Chama a função de inserir o novo filme, encaminha os dados e o content-type
+    let filme = await filmeController.inserirFilme(dadosBody, contentType)
+    
+    response.status(filme.status_code)
+    response.json(filme)
+    console.log('ENDPOINT 3° - Requisitado na tbl_filme')
+})
+// 4° ATUALIZAR
+router.put('/:id', cors(), bodyParserJSON, async function (request, response){
+    // Recebe o ID encaminhado via parametro na requisição
+    let idFilme = request.params.id
+    // Recebe os dados do body da requisição (Se você utilizar o bodyParser, é obrigatório ter no endPoint)
+    let dadosBody = request.body
+    let contentType = request.headers['content-type'] 
+    // Chama a função de inserir o novo filme, encaminha os dados e o content-type
+    let filme = await filmeController.atualizarFilme(dadosBody, contentType, idFilme)
+    
+    response.status(filme.status_code)
+    response.json(filme)
+    console.log('ENDPOINT 4° - Requisitado na tbl_filme')
+})
+// 5° DELETAR
+router.delete('/:id', cors(), async function (request, response){
+    // Recebe o ID encaminhado via parametro na requisição
+    let idFilme = request.params.id
+    // Chama a função de buscar filme por ID
+    let filme = await filmeController.excluirFilme(idFilme)
+    response.status(filme.status_code)
+    response.json(filme)
+    console.log('ENDPOINT 5° - Requisitado na tbl_filme')
+})
 module.exports = router
