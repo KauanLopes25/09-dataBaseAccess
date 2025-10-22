446dcdbc01b2f38c886174d074638eea0c5d6ff9
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-10-22 10:11:56 -0300

    Adicionado arquivo de rotas para acesso com a tabela filme

---
+++ controller/filme/router_filme.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/********************************************************************************************
+* Objetivo: Arquivo responsavel pela configuração das rotas para a tabela filme.
+* Autor: Kauan Lopes Pereira
+* Data: 22/10/2025
+* Versão: 1.0.10.25
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+
+************************************** OBSERVAÇÕES ******************************************
+
+******************************** BIBLIOTECAS UTILIZADAS *************************************
+
+********************************************************************************************/
+// Importação do arquivo model da tbl_filme
+const filmeController = require('../../controller/filme/controller_filme.js')
+const DEFAULT_MESSAGES = require('../modulo/config_messages.js')
